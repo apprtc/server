abde2b9f5ad034c3efb8a8cc6afc8d347e335296
aojianwu <aojianwu@hotmail.com>
2019-01-09 10:08:29 +0800

    lanuch.json

--- apps/focusteachrtcd/main.go
+++ apps/focusteachrtcd/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 
 	"github.com/apprtc/server/cmd/signalsvc"
-	"github.com/apprtc/server/cmd/stunsvc"
 	"github.com/go-apps/pkg/core"
 )
 
@@ -28,8 +27,8 @@ func Main() {
 	signalServer := signalsvc.NewSignalServer(signalIp, signalPort)
 	core.CoreApplication().AddTask(signalServer)
 
-	stunServer := stunsvc.NewStunServer(stunIp, stunPort)
-	core.CoreApplication().AddTask(stunServer)
+	// stunServer := stunsvc.NewStunServer(stunIp, stunPort)
+	// core.CoreApplication().AddTask(stunServer)
 }
 
 func main() {
