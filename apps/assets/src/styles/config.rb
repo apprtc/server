262df1f2cfd0724db79043d9b690986e799df3c5
aojianwu <aojianwu@hotmail.com>
2019-01-14 15:41:40 +0800

    change dir

---
+++ apps/assets/src/styles/config.rb
@@ -0,0 +1,6 @@
+project_path = "../../dist/static/css"
+images_dir = "../../dist/static/img"
+sass_dir = "../../dist/static/css"
+fonts_dir = "../../dist/static/fonts"
+
+relative_assets = true
--- cmd/signalsvc/httphandle.go
+++
@@ -1,5 +0,0 @@
-package signalsvc
-
-func httpHandle() {
-
-}
